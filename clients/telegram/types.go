f8fd9dc89eb43342e0c8eaf302d12f58eaa96cb7
Евгений Клюкман <kef1991@yandex.ru>
2024-08-13 21:21:34 +1000

    method Fetch has done

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -2,11 +2,25 @@ package telegram
 
 // Структура с API телеграм-бота https://core.telegram.org/bots/api#getting-updates
 type Update struct {
-	ID      int    `json:"update_id"`
-	Message string `json:"message"`
+	ID      int              `json:"update_id"`
+	Message *IncomingMessage `json:"message"` //Поле Message может отсутствовать, и мы получим nil. Поэтому ссылка на структуру
 }
 
 type UpdatesResponse struct {
 	Ok     bool     `json:"ok"`
 	Result []Update `json:"result"`
+}
+
+type IncomingMessage struct {
+	Text string `json:"text"`
+	From From   `json:"from"`
+	Chat Chat   `json:"chat"`
+}
+
+type From struct {
+	Username string `json:"username"`
+}
+
+type Chat struct {
+	ID int `json:"id"`
 }
--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,6 +1,8 @@
 package telegram
 
 import (
+	"errors"
+
 	"github.com/jedyEvgeny/simple_telegram_bot/clients/telegram"
 	"github.com/jedyEvgeny/simple_telegram_bot/events"
 	"github.com/jedyEvgeny/simple_telegram_bot/lib/e"
@@ -13,6 +15,17 @@ type Proceccor struct {
 	storage storage.Storage
 }
 
+type Meta struct {
+	ChatID   int
+	Username string
+}
+
+var (
+	ErrUncknownEventType = errors.New("неизвестный тип события")
+	ErrEmptyUpdates      = errors.New("внутренняя ошибка")
+	ErrUncknownMeta      = errors.New("неизвестный тип Meta")
+)
+
 func New(client *telegram.Client, storage storage.Storage) *Proceccor {
 	return &Proceccor{
 		tg:      client,
@@ -21,33 +34,88 @@ func New(client *telegram.Client, storage storage.Storage) *Proceccor {
 }
 
 func (p *Proceccor) Fetch(limit int) ([]events.Event, error) {
-	//С помощью клиента получаем все апдейты
-	update, err := p.tg.Updates(p.offset, limit)
+	//С помощью клиента получаем все апдейты (апдейты характерны именно для мессенджера Telegram)
+	updates, err := p.tg.Updates(p.offset, limit)
 	if err != nil {
 		return nil, e.Wrap("не смогли получить события", err)
 	}
 
-	//Аллоцируем память под результат
-	res := make([]events.Event, 0, len(update))
+	//Возвращаем нулевой результат, если не нашли апдейтов
+	if len(updates) == 0 {
+		return nil, e.Wrap("список апдейтов пустой:", ErrEmptyUpdates)
+	}
 
-	//Обходим все апдейты и преобразуем их в эвенты
-	for _, u := range update {
+	//Аллоцируем память под результат. Тип Event - более общая сущность, чем апдейты
+	//В Event мы можем преобразовывать всё что получаем от потенциальных других мессенджеров
+	//в каком бы формате они не предоставляли информацию
+	res := make([]events.Event, 0, len(updates))
+
+	//Перебираем все апдейты и преобразуем их в тип Event пакета events
+	for _, u := range updates {
 		e := event(u)
 		res = append(res, e)
 	}
+
+	//Обновляем значение внутреннего поля offset, чтобы в следующий раз получить следующую пачку изменений
+	p.offset = updates[len(updates)-1].ID + 1
+
+	return res, nil
+}
+
+// Process выполняет различные действия в зависимости от типа эвента
+func (p *Proceccor) Process(event events.Event) error {
+	switch event.Type {
+	case events.Message: //когда работаем с сообщением
+		p.ProcessMessage(event)
+	default: //Когда не знаем с чем работаем
+		return e.Wrap("не смогли выполнить сообщение", ErrUncknownEventType)
+	}
+}
+
+func (p *Proceccor) ProcessMessage(event events.Event) {
+	meta, err := meta(event)
+	if err != nil {
+		return e.Wrap("не смогли выполнить сообщение", err)
+	}
+}
+
+func meta(event events.Event) (Meta, error) {
+	res, ok := event.Meta.(Meta)
+	if !ok {
+		return Meta{}, e.Wrap("не смогли получить Meta")
+	}
+	return res, nil
 }
 
 func event(upd telegram.Update) events.Event {
+	updType := fetchType(upd)
+
 	res := events.Event{
 		Type: fetchType(upd),
 		Text: fetchText(upd),
 	}
-}
 
-func fetchType(upd telegram.Update) string {
+	if updType == events.Message {
+		res.Meta = Meta{
+			ChatID:   upd.Message.Chat.ID,
+			Username: upd.Message.From.Username,
+		}
+	}
 
+	return res
 }
 
 func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+	return upd.Message.Text
+}
 
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown //Если сообщение нулевое, то тип неизвестен
+	}
+	//Если сообщение не нулевое, то событие - это сообщение
+	return events.Message
 }
